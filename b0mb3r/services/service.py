608c613afe5a48c5f2b100c28979be669840bb44
Nikita <42045258+crinny@users.noreply.github.com>
2020-01-21 21:55:00 +0300

    Added imba services

--- b0mb3r/services/service.py
+++ b0mb3r/services/service.py
@@ -2,7 +2,7 @@ import aiohttp
 
 
 class Service:
-    user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
     client = aiohttp.ClientSession()
 
     def __init__(self, phone, phone_code):
