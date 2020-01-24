d4da7df84189f37fb56398947f1cff0ee645b57d
Nikita <42045258+crinny@users.noreply.github.com>
2020-01-24 23:42:02 +0300

    'Cause baby tonight, the creeper's trying to steal all our stuff again

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 NAME = "b0mb3r"
 DESCRIPTION = "Открытый и бесплатный СМС бомбер"
 URL = "https://github.com/crinny/b0mb3r"
-EMAIL = ""
+EMAIL = "b0mb3r.abuse@gmail.com"
 AUTHOR = "crinny"
 REQUIRES_PYTHON = ">=3.7.0"
 VERSION = "2.1.9"
