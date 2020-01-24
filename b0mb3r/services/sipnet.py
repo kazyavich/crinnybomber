5932aba5984589cd0ae8bb54892df489815dae5f
Nikita <42045258+crinny@users.noreply.github.com>
2020-01-25 00:15:18 +0300

    Update sipnet.py

--- b0mb3r/services/sipnet.py
+++ b0mb3r/services/sipnet.py
@@ -1,10 +1,9 @@
-# Работает раз в час на номер. А так вообще классный сервис, стоит добавить.
-
 from service import Service
 
 
 class SipNet(Service):
     async def run(self):
         await self.post(
-            f"https://register.sipnet.ru/cgi-bin/exchange.dll/RegisterHelper?oper=9&callmode=1&phone=%2B{self.formatted_phone}",
+            f"https://register.sipnet.ru/cgi-bin/exchange.dll/RegisterHelper",
+            params={"oper": 9, "callmode": 1, "phone": "+" + self.formatted_phone},
         )
