ee1e4cb8a4f2743210dcd76dc1d191fd7a3c9984
Dmitry Pavlov <dmitrypavlov.design@gmail.com>
2020-02-14 18:51:05 +0200

    add a second request to service

--- b0mb3r/services/qlean.py
+++ b0mb3r/services/qlean.py
@@ -1,9 +1,20 @@
 from service import Service
+from random import randint
 
 
 class Qlean(Service):
     async def run(self):
         await self.post(
             "https://qlean.ru/clients-api/v2/sms_codes/auth/request_code",
+            headers={"User-Agent": self.user_agent},
             json={"phone": self.formatted_phone},
+        )
+        await self.get(
+            "https://sso.cloud.qlean.ru/http/users/requestotp",
+            headers={"Referer": "https://qlean.ru/sso?redirectUrl=https://qlean.ru/"},
+            params={
+                "phone": self.formatted_phone,
+                "clientId": "undefined",
+                "sessionId": str(randint(5000, 9999))
+            }
         )
