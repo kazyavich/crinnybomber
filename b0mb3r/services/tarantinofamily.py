349433b1a1659563b7b6f12373ab04b2ccb40413
Dmitry Pavlov <dmitrypavlov.design@gmail.com>
2020-02-16 16:59:04 +0200

    +4 services

---
+++ b0mb3r/services/tarantinofamily.py
@@ -0,0 +1,12 @@
+from service import Service
+
+
+class TarantinoFamily(Service):
+    async def run(self):
+        if self.phone_code == "380":
+            await self.post("https://www.tarantino-family.com/wp-admin/admin-ajax.php",
+                data={
+                    "action": "callback_phonenumber",
+                    "phone": self.formatted_phone
+                }
+            )
