4ca7c0a44572a7c0e9b96bbc96c9650cb68148c3
Nikita <isfellop@gmail.com>
2020-03-21 11:02:29 +0300

    Delete dodopizza.py

--- b0mb3r/services/dodopizza.py
+++
@@ -1,9 +0,0 @@
-from service import Service
-
-
-class Dodopizza(Service):
-    async def run(self):
-        await self.post(
-            f"https://mapi.dodopizza.ru/api/v1/login/sendPinCode?phoneNumber=+{self.formatted_phone}&Language=ru-RU&CountryCode=643",
-            data='',
-        )
