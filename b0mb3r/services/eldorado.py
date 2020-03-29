78f41e6567accea7333417cb39a34883cf8210f8
crinny <isfellop@gmail.com>
2020-03-29 18:48:54 +0300

    :ambulance: Hotfix for Ukraine (ahhhh that's hot)

--- b0mb3r/services/eldorado.py
+++ b0mb3r/services/eldorado.py
@@ -5,23 +5,13 @@ class EldoradoUA(Service):
     phone_codes = ["380"]
 
     async def run(self):
-        await self.options(
-            "https://api.eldorado.ua/v1/sign/",
-            params={
-                "login": self.formatted_phone,
-                "step": "phone-check",
-                "fb_id": None,
-                "fb_token": None,
-                "lang": "ru",
-            },
-        )
         await self.get(
             "https://api.eldorado.ua/v1/sign/",
             params={
                 "login": self.formatted_phone,
                 "step": "phone-check",
-                "fb_id": None,
-                "fb_token": None,
+                "fb_id": "null",
+                "fb_token": "null",
                 "lang": "ru",
             },
         )
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ URL = "https://github.com/crinny/b0mb3r"
 EMAIL = ""
 AUTHOR = "crinny"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "2.5.6"
+VERSION = "2.5.6.1"
 
 REQUIRED = ["aiohttp", "phonenumbers", "click", "sentry-sdk"]
 
