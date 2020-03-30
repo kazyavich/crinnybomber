a182ca1d80336bb236a4fa664fb9c9c59bb3c04b
crinny <isfellop@gmail.com>
2020-03-30 14:38:37 +0300

    :sparkles: +1 service

---
+++ b0mb3r/services/izi.py
@@ -0,0 +1,18 @@
+from service import Service
+
+
+class IziUA(Service):
+    phone_codes = ["380"]
+
+    async def run(self):
+        await self.post(
+            "https://izi.ua/api/auth/register",
+            json={
+                "phone": "+" + self.formatted_phone,
+                "name": self.russian_name,
+                "is_terms_accepted": True,
+            },
+        )
+        await self.post(
+            "https://izi.ua/api/auth/sms-login", json={"phone": "+" + self.formatted_phone,}
+        )
